4bd92218e527e5240c35c0881b3f549b3700bd09
Evgen Tikhonov <tihonovaelena1503@gmail.com>
2019-03-27 23:15:59 +0300

    Update sript.js

--- js/sript.js
+++ js/sript.js
@@ -1,3 +1,9 @@
 menu.onclick = function myFunction() {
   var x = document.getElementById("myTopnav");
+
+  if(x.className === "topnav") { /*Отображение иконки при окне не более 768px*/
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
